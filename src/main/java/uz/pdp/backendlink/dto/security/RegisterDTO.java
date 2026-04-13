df0919e232360b613571aaedb36934b60306dc47
Ulug'bek <ulugbekmominov535@gmail.com>
2026-04-13 18:54:47 +0500

    email and password validation

--- src/main/java/uz/pdp/backendlink/dto/security/RegisterDTO.java
+++ src/main/java/uz/pdp/backendlink/dto/security/RegisterDTO.java
@@ -1,5 +1,8 @@
 package uz.pdp.backendlink.dto.security;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,13 @@ public class RegisterDTO {
 
     private String username;
 
+    @NotBlank(message = "Parol bo'sh bo'lmasligi kerak")
     private String password;
 
+    @NotBlank(message = "Email bo'sh bo'lmasligi kerak")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Iltimos, haqiqiy email manzilini kiriting"
+    )
     private String email;
 }
